62c49ba7ba3a5a403ffe09e66846ffda47b8b728
Iroveena Pagsibigan and Tram Dinh <iroveena@>
2017-05-22 20:07:40 -0700

    Add light and dark background buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 $("document").ready(function() {
   $(".clickable").click(function() {
-    $("#initially-showing").slideToggle();
-    $("#initially-hidden").slideToggle();
+    $(".initially-showing").slideToggle();
+    $(".initially-hidden").slideToggle();
+  });
+
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+    $("body").addClass("white-text");
+  });
+
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+    $("body").addClass("black-text");
   });
 });
